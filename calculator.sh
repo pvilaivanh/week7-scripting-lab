a1f785fa50e71ec255c3aacab0ae7eaa3d44b2ce
Punoi Vilaivanh <pvilaivanh@gmail.com>
2026-03-10 14:31:37 -0400

    Added multiplication feature

--- calculator.sh
+++ calculator.sh
@@ -5,7 +5,7 @@ read -p "Enter first number: " num1
 read -p "Enter second number: " num2
 
 sum=$((num1 + num2))
+product=$((num1 * num2))
 
-echo "----------------------"
-echo "Result: $num1 + $num2 = $sum"
-echo "----------------------"
+echo "Addition: $sum"
+echo "Multiplication: $product"
