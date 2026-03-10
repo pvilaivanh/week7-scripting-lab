62d6c6c7ff61fce3e32771c3ad35dbd5779ac68f
Punoi Vilaivanh <pvilaivanh@gmail.com>
2026-03-10 12:49:44 -0400

    Added calculator script

---
+++ calculator.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Simple Calculator"
+
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+
+sum=$((num1 + num2))
+
+echo "----------------------"
+echo "Result: $num1 + $num2 = $sum"
+echo "----------------------"
